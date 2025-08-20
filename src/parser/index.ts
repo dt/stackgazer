953126dbfc9c329ceb7e80eefbf0d5f80aad2874
David Taylor <davidt@davidt.io>
2025-08-20 16:43:02 +0000

    rip out web worker parser

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -3,15 +3,12 @@
  *
  * Public API:
  * - FileParser: Synchronous parsing
- * - AsyncFileParser: Asynchronous parsing with web workers
  * - ZipHandler: Zip file utilities
  */
 
 export * from './types.js';
 export { FileParser } from './parser.js';
-export { AsyncFileParser } from './async-parser.js';
 export { ZipHandler } from './zip.js';
 
 // Convenient aliases
 export { FileParser as Parser } from './parser.js';
-export { AsyncFileParser as AsyncParser } from './async-parser.js';
